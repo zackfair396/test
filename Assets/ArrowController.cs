7aa13d01b2004c8cde16a8218694b6da4803a174
SEIKO_MWAM <zack.fair.l.t@gmail.com>
2020-08-16 03:49:28 +0900

    Unity教科書の素材。GitHubの練習用。Unityでのシーンも有。

---
+++ Assets/ArrowController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour
+{
+    GameObject player;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Hit");
+        Destroy(gameObject);
+
+        GameObject director = GameObject.Find("GameDirector");
+        director.GetComponent<GameDirector>().DecreaseHP();
+
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.player = GameObject.Find("player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+       /* transform.Translate(0, -0.1f, 0); */
+
+        if(transform.position.y < -5.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public void LBottownDown()
+    {
+        transform.Translate(-3, 0, 0);
+    }
+
+    public void RBottownDown()
+    {
+        transform.Translate(3, 0, 0);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
